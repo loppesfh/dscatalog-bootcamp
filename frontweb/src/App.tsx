579ff75f28be57d5cd2f3c00b2c3b65f1cea3b0f
Loppes Filho <loppesfh@vostro.megganet.com.br>
2021-07-10 16:32:32 -0300

    Routes

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,12 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Home from 'pages/Home';
+
+import Routes from 'Routes';
 
 const App = () => {
   return (
     <>
-      <Home />
-      <h1>Hello DSCatalog</h1>
+      <Routes />
     </>
   );
 };
