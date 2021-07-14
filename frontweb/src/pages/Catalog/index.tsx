ff7642f339165e54730d9d569baa5f4f2ee42319
Loppes Filho <loppesfh@vostro.megganet.com.br>
2021-07-13 23:05:38 -0300

    FormatPrice

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,4 +1,4 @@
-import { Product } from 'assets/types/product';
+import { Product } from 'types/product';
 import ProductCard from 'components/ProductCard';
 
 const Catalog = () => {
@@ -7,7 +7,7 @@ const Catalog = () => {
     "id": 2,
     "name": "Smart TV",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "price": 2190.57,
+    "price": 2190,
     "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
     "date": "2020-07-14T10:00:00Z",
     "categories": [
