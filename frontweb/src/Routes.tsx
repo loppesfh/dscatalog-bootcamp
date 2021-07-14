afff3b9cf71c4689574b3c86afc71a61a3b07ba1
Loppes Filho <loppesfh@vostro.megganet.com.br>
2021-07-14 06:15:07 -0300

    ProductDetails route

--- frontweb/src/Routes.tsx
+++ frontweb/src/Routes.tsx
@@ -4,6 +4,8 @@ import Home from "pages/Home";
 import Navbar from "components/Navbar";
 import Catalog from "pages/Catalog";
 import Admin from "pages/Admin"
+import ProductDetails from "pages/ProductDetails";
+
 const Routes = () => {
   return (
     <BrowserRouter>
@@ -12,8 +14,11 @@ const Routes = () => {
         <Route path="/" exact>
            <Home />
         </Route>
-        <Route path="/products">
+        <Route path="/products" exact>
            <Catalog />
+        </Route>
+        <Route path="/products/:productId">
+          <ProductDetails />        
         </Route>
         <Route path="/admin">
            <Admin />
