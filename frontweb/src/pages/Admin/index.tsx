25078228e3d5eb07abdc36afffc9527ab270dc8e
Loppes Filho <loppesfh@vostro.megganet.com.br>
2021-07-18 17:53:53 -0300

    Routes, redirecting e nesting

--- frontweb/src/pages/Admin/index.tsx
+++ frontweb/src/pages/Admin/index.tsx
@@ -1,3 +1,4 @@
+import { Switch, Route } from 'react-router';
 import Navbar from './Navbar';
 import './styles.css';
 
@@ -6,7 +7,17 @@ const Admin = () => {
     <div className="admin-container">
       <Navbar />
       <div className="admin-content">
-        <h1>Conteudo</h1>
+        <Switch>
+          <Route path="/admin/products">
+            <h1>Product CRUD</h1>
+          </Route>
+          <Route path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </Route>
+          <Route path="/admin/users">
+            <h1>user CRUD</h1>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
