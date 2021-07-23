450df609e7a819984178814734300f3a7d75b641
Loppes Filho <loppesfh@vostro.megganet.com.br>
2021-07-23 20:53:50 -0300

    Auth routes

---
+++ frontweb/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,31 @@
+import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
+import { Switch, Route } from 'react-router';
+
+const Auth = () => {
+    return (
+        <div className="auth-container">
+            <div className="auth-banner-container">
+                <h1>Divulgue seus produtos no DS Catalog</h1>
+                <p>Faça parte de nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
+                <AuthImage />
+            </div>
+            <div className="auth-form-container">
+                <Switch>
+                    <Route path="/admin/auth/login">
+                        <h1>Card de Login</h1>
+                    </Route>
+                    <Route path="/admin/auth/signup">
+                        <h1>Card de Signup</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+                        <h1>Card de Recover</h1>
+                    </Route>
+                </Switch>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Auth;
